400344fe38df6a154f165d5d95de11d25a28e3a7
Igor Belo <igorcoura@gmail.com>
2016-11-01 11:30:00 -0200

    add function to save images step by step

--- line_detection.py
+++ line_detection.py
@@ -9,6 +9,8 @@ import networkx as nx
 from skimage.morphology import square, closing
 
 import community
+import sys
+import os
 
 Y_DISTANCE = 5
 
@@ -20,10 +22,9 @@ def measure_labels(image):
 
     block_size = 41
     binary_adaptive = threshold_adaptive(work_image, block_size, offset=10)
-    # binary_adaptive = closing(binary_adaptive, square(3))
     segmented_image = measure.label(binary_adaptive, background=1)
 
-    return segmented_image
+    return (binary_adaptive, segmented_image)
 
 def plot_centroids(image, regions):
     fig, ax = plt.subplots()
@@ -38,38 +39,104 @@ def plot_centroids(image, regions):
 
 def create_nodes_from_regions(graph, regions):
     for label, prop in enumerate(regions):
-        graph.add_node(label, pos=prop.centroid)
+        row, col = prop.centroid
+        graph.add_node(label, pos=(col, row))
 
 def create_edges_from_regions(graph, regions):
     for label, properties in enumerate(regions):
-        [graph.add_edge(label, destination_label) for destination_label, destination_properties in enumerate(regions) if abs(properties.centroid[1] - destination_properties.centroid[1]) <= Y_DISTANCE]
+        [graph.add_edge(label, destination_label) for destination_label, destination_properties in enumerate(regions) if abs(properties.centroid[0] - destination_properties.centroid[0]) <= Y_DISTANCE]
 
-def draw_best_partition(graph):
-    partition = community.best_partition(graph)
-
+def draw_best_partition(partition, graph):
     #drawing
+    pos = nx.get_node_attributes(graph, 'pos')
     size = float(len(set(partition.values())))
-    pos = nx.spring_layout(graph)
     count = 0.
     for com in set(partition.values()) :
         count = count + 1.
         list_nodes = [nodes for nodes in partition.keys()
                                 if partition[nodes] == com]
         nx.draw_networkx_nodes(graph, pos, list_nodes, node_size = 20,
-                           node_color = str(count / size))
+                               node_color = str(count / size))
 
-    nx.draw_networkx_edges(graph, pos, alpha=0.5)
+    nx.draw_networkx_edges(graph, pos, edge_color='#ff0000', alpha=0.1, width=1)
 
 def draw_graph(graph):
-    nx.draw(graph, nx.get_node_attributes(graph, 'pos'), node_size=50, font_size=9)
+    nx.draw(graph, nx.get_node_attributes(graph, 'pos'), node_size=25)
 
-# image = data.page()
-image = imread("images/1.jpeg")
-segmented_image = measure_labels(image)
+def fill_lines(partition):
+    from matplotlib.patches import Rectangle
+
+    currentAxis = plt.gca()
+    last_max_x = None
+    last_community = None
+    for element_index, community_index in partition.items():
+        if last_community is not None and last_community != community_index:
+            last_max_x = None
+
+        prop = regions[element_index]
+        min_y, min_x, max_y, max_x = prop.bbox
+
+        if last_max_x is not None:
+            min_x = last_max_x
+
+        currentAxis.add_patch(
+            Rectangle(
+                (min_x, min_y),
+                max_x - min_x,
+                max_y - min_y,
+                alpha=0.1,
+                facecolor=colors[community_index],
+                edgecolor='none'
+            )
+        )
+        last_max_x = max_x
+        last_community = community_index
+
+from skimage import img_as_uint
+def generate_step_images(image, binary_image, segmented_image, graph, partition):
+    path = "steps/%s" % sys.argv[1]
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    plt.imshow(binary_image)
+    plt.savefig("%s/2-binary-image.jpg" % path)
+    plt.clf()
+    plt.imshow(segmented_image)
+    plt.savefig("%s/3-segmented-image.jpg" % path)
+    plt.clf()
+    plt.imshow(image)
+    plt.savefig("%s/1-original-image.jpg" % path)
+    draw_best_partition(partition, graph)
+    plt.savefig("%s/4-graph.jpg" % path)
+    plt.clf()
+    plt.imshow(image)
+    fill_lines(partition)
+    plt.savefig("%s/5-partition.jpg" % path)
+
+image = imread("images/%s" % sys.argv[1])
+binary_image, segmented_image = measure_labels(image)
 regions = measure.regionprops(segmented_image)
 G = nx.Graph()
 create_nodes_from_regions(G, regions)
 create_edges_from_regions(G, regions)
-# draw_graph(G)
-draw_best_partition(G)
-plt.show()
+partition = community.best_partition(G)
+
+from random import randint
+colors = ['#%06X' % randint(0, 0xFFFFFF) for _ in range(len(set(partition.values())))]
+generate_step_images(
+    image,
+    binary_image,
+    segmented_image,
+    G,
+    partition
+)
+
+# print len(regions)
+# print partition
+
+# print segmented_image[300]
+
+# plt.imshow(image)
+# fill_lines(partition)
+
+# plt.show()
