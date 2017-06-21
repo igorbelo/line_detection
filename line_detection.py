ba13008cdf7079d156cc0623b95360aa04541424
Igor Belo <igorcoura@gmail.com>
2017-06-21 16:31:39 -0300

    add overlap approach

--- line_detection.py
+++ line_detection.py
@@ -14,6 +14,11 @@ import sys
 from ground_truth_reader import build_line_meta
 import draw
 import cv2
+import peakutils
+from peakutils.plot import plot as pplot
+
+OVERLAP_RATIO = 0.5
+Y_DISTANCE_THRESHOLD = 5 # in pixels
 
 def measure_labels(image):
     if image.ndim > 2:
@@ -26,10 +31,10 @@ def measure_labels(image):
     segmented_image, num_labels = measure.label(
         binary_adaptive, background=1, return_num=True)
 
-    # other_image = morphology.remove_small_objects(segmented_image, 10)
-    # segmented_other_image = measure.label(other_image, background=0)
+    clean_image = morphology.remove_small_objects(segmented_image, 10)
+    segmented_clean_image = measure.label(clean_image, background=0)
 
-    return (binary_adaptive, (segmented_image, num_labels))
+    return (binary_adaptive, (segmented_clean_image, num_labels))
 
 def create_mask(binary_image):
     return np.full_like(binary_image_uint, 255, dtype=np.uint8)
@@ -40,7 +45,7 @@ def put_component_on_mask(mask, coords):
 
 def distance_between_two_components(d1, c2, c2_coords):
     y, x = np.hsplit(c2_coords, 2)
-    return min(d1[y, x])
+    return d1[y, x].min()
 
 def create_nodes(graph, regions):
     for prop in regions:
@@ -50,16 +55,24 @@ def generate_graph(distances, regions):
     G = nx.Graph()
     create_nodes(G, regions)
     flat_distances = [item for sublist in distances for item in sublist]
-    threshold = np.percentile(flat_distances, 1.5)
+    threshold = Y_DISTANCE_THRESHOLD
     for props in regions[:-1]:
         label = props.label
         index = label-1
         distances_from_component = distances[index]
         for j, d in enumerate(distances_from_component, label+1):
-            if d <= threshold:
+            if overlap_ratio(props, regions[j-1]) >= OVERLAP_RATIO or d <= threshold:
                 G.add_edge(label, j)
 
     return G
+
+def overlap_ratio(region1, region2):
+    y1top, _, y1bot, _ = region1.bbox
+    y2top, _, y2bot, _ = region2.bbox
+    l1 = range(y1top, y1bot)
+    l2 = range(y2top, y2bot)
+    ints = len(set(l1).intersection(l2))
+    return float(ints) / min(len(l1), len(l2))
 
 def get_distances_between_components(binary_image, regions):
     distances = []
@@ -85,6 +98,7 @@ def calculate_accuracy(ground_truth, regions, communities):
     total_lines = len(ground_truth)
     total_components = len(regions)
     found = 0
+    print ground_truth
     for label, community in communities.iteritems():
         if community < total_lines:
             line = ground_truth[community]
@@ -93,13 +107,11 @@ def calculate_accuracy(ground_truth, regions, communities):
                x_left >= line["xLeft"] and x_right <= line["xRight"]:
                 found += 1
 
-    print (float(found) / total_components) * 100.0
-
-
+    return (float(found) / total_components) * 100.0
 
 if __name__ == '__main__':
     base_dir = sys.argv[1]
-    for i in range(30):
+    for i in range(3, 4):
         filename = '%s-%s' % (base_dir, str(i).zfill(3))
         image = imread("images/%s/%s.png" % (base_dir, filename))
         ground_truth = build_line_meta("ground-truth/%s/%s.json" % (base_dir, filename))
@@ -113,7 +125,8 @@ if __name__ == '__main__':
         if distances:
             G = generate_graph(distances, regions)
             communities = community.best_partition(G)
-            # result = calculate_accuracy(ground_truth, regions, communities)
+            result = calculate_accuracy(ground_truth, regions, communities)
+            print str(i).zfill(3), result
             draw.result(segmented_image, communities, 'results/%s' % base_dir, filename+'.png', 'png')
             draw.generate_step_images(image, binary_image, segmented_image, G, communities, regions, 'steps/%s/%s' % (base_dir, filename))
 
